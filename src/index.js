3150d2a5c43a1a397b3276b5616684b71686044f
lisandromoura <lisandro.cabrom@gmail.com>
2024-02-18 16:48:07 -0300

    feat:✨ Novos ajustes
    apenas ajustes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ console.log("Commit2 master")
 console.log("Commit3 master")
 
 
-/////////////////////////////////////////////
-// bloco de funções criadas
+
 const featureFeitaPeloProgramador2 = () => {
     console.log("feature2 feita pelo programador 2")
 }
@@ -23,18 +22,15 @@ const bomDiaBatata = (nome) => {
 
 
 
-// 3-função-para-dar-bom-dia-para-a-skate
+
 const bomDiaSkate = () => {
     console.log("Bom dia linda Skate do papai")
 }
 
-// 5-cantar-musica-para-pipi 
 const cantarMusicaParaPipi = () => {
     console.log("Pia pipi,\n pia pip faz\n pipi pipi")
 }
 
-/////////////////////////////////////////////
-// execuções:
 featureFeitaPeloProgramador2()
 executar()
 bomDia("Baby")
